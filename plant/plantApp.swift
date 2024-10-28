11af5060fe7e842fd2e8142fd18e62a6cef9372f
RaneemAlomair <raneemalomair11@gmail.com>
2024-10-28 23:07:57 +0300

    Initial Commit

---
+++ plant/plantApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantApp.swift
+//  plant
+//
+//  Created by Raneem Alomair on 25/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct plantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
